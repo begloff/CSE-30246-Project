0b6475bee42efb11a41c20730ce66d9ed42e8358
begloff <begloff@nd.edu>
2022-09-30 11:57:11 -0400

    New Deployment: Fix

---
+++ dist/js/about.6d5e3e32.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkdb_proj"]=self["webpackChunkdb_proj"]||[]).push([[443],{7381:function(n,r,u){u.r(r),u.d(r,{default:function(){return d}});var t=u(3396);const a={class:"about"},e=(0,t._)("h1",null,"This is an about page",-1),s=[e];function c(n,r){return(0,t.wg)(),(0,t.iD)("div",a,s)}var o=u(89);const i={},b=(0,o.Z)(i,[["render",c]]);var d=b}}]);
+//# sourceMappingURL=about.6d5e3e32.js.map

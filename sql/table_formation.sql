c525865f58ba12d5d3991b69ed6e110f2d389856
begloff <begloff@nd.edu>
2022-10-30 19:34:28 -0400

    Added Schedule

--- sql/table_formation.sql
+++ sql/table_formation.sql
@@ -1,10 +1,18 @@
-CREATE TABLE nightly_stats(
+-- CREATE TABLE nightly_stats(
+--    week_id int(11),
+--    day_of_week char(2),
+--    date datetime,
+--    total_revenue decimal(5,2),
+--    cash_revenue decimal(5,2),
+--    venmo_revenue decimal(5,2),
+--    online_fee decimal(5,2),
+--    num_orders int,
+-- );
+
+CREATE TABLE schedule(
    week_id int(11),
    day_of_week char(2),
-   date datetime,
-   total_revenue decimal(5,2),
-   cash_revenue decimal(5,2),
-   venmo_revenue decimal(5,2),
-   online_fee decimal(5,2),
-   num_orders int,
+   w1 char(20),
+   w2 char(20),
+   w3 char(20)
 );

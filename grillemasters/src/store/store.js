faf0d350f5631f8f039b81aa7642fb3da03577df
begloff <begloff@nd.edu>
2022-11-18 13:12:09 -0500

    Removed Unnecessary function

--- grillemasters/src/store/store.js
+++ grillemasters/src/store/store.js
@@ -11,33 +11,9 @@ export default createStore({
   state: {
 
     user: null,
-    orders: [],
-    nightTotal: null,
-    venmo: null,
-    cash: null,
-    days: [],
     weeks: null, //SQL
     sWeek: null, //SQL
     customerBase: {}, //SQL
-    weeklyRev: null,
-    weeklyCashRev: null,
-    weeklyVenmoRev: null,
-    weeklyOnlineFee: null,
-    nightOnlineFee: null,
-    selectedWeek: weeklyPrefix,
-    storeRuns: [],
-    totalCost: null,
-    weeksOfOperation: [],
-    schedule: [],
-    employees: [],
-    hours: [],
-    workingEmployees: [],
-    totalHours: null,
-    employeeWages: [],
-    employeeRole: null,
-    customer: null,
-    customerBase: [],
-    queue: [],
 
   },
   mutations: {
@@ -47,60 +23,9 @@ export default createStore({
     CLEAR_USER(state) {
       state.user = null;
     },
-    SET_ORDERS(state,payload){
-      state.orders = payload.o;
-      state.nightTotal = payload.t;
-      state.nightOnlineFee = payload.of;
-    },
-    SET_CHART_DATA(state,payload){
-      state.venmo = payload.v;
-      state.cash = payload.c;
-      state.days = payload.w;
-      state.weeklyRev = payload.r;
-      state.weeklyVenmoRev = payload.vr;
-      state.weeklyCashRev = payload.cr;
-      state.weeklyOnlineFee = payload.of;
-    },
-    SET_STORE_DATA( state, payload ){
-      state.storeRuns = payload.s;
-      state.totalCost = payload.c;
-    },
-    SET_WEEKS(state,payload){
-      state.weeksOfOperation = payload;
-    },
-    SET_SCHEDULE(state,payload){
-      state.schedule = payload;
-    },
-    ADD_STORE_RUN(state,payload){
-      state.storeRuns.push(payload)
-    },
-    INCREMENT_COST(state,payload){
-      state.totalCost += payload
-    },
-    DEL_STORE_RUN(state,payload){
-      state.storeRuns = state.storeRuns.filter( x => x.id != payload)
-    },
-    CHANGE_SELECTED_WEEK(state,payload){
-      state.selectedWeek = payload
-    },
-    SET_EMPLOYEES(state,payload){
-      state.employees = payload
-    },
-    SET_HOURS(state,payload){
-      state.hours = payload.h
-      state.workingEmployees = payload.e
-      state.totalHours = payload.t
-      state.employeeWages = payload.p
-    },
     SET_EMPLOYEE_ROLE(state,payload){
       state.employeeRole = payload.s
       state.customer = payload.n
-    },
-    SET_CUSTOMER_BASE(state,payload){
-      state.customerBase = payload.sort();
-    },
-    SET_QUEUE(state,payload){
-      state.queue = payload;
     },
     SET_WEEKS(state,payload){
       state.weeks = payload
@@ -216,12 +141,6 @@ export default createStore({
               await this.dispatch('getRole')
               if(this.state.employeeRole){
 
-                await this.dispatch('getFinancialData')
-                await this.dispatch('getStoreData')
-                await this.dispatch('getWeeksOfOperation')
-                await this.dispatch('getSchedule')
-                await this.dispatch('getHours')
-                await this.dispatch('getCustomerBase')
                 await this.dispatch('getWeeksSQL')
                 await this.dispatch('getCustomerBase')
 
@@ -233,7 +152,6 @@ export default createStore({
               } else {
                 if (router.isReady() && router.currentRoute.value.path == '/login'){
                   const queue = await getDoc(doc(db,"orders","daily-queue"))
-                  commit("SET_QUEUE",queue.data().queue);
                   router.push('/customer')
               }
               }
@@ -250,257 +168,6 @@ export default createStore({
           commit("SET_EMPLOYEE_ROLE", {s: false, n: person.data().name})
         }
      },
-
-     getOrders({ commit }){
-      const q = query( ordersCollection, orderBy("date") )
-
-      onSnapshot( q, (querySnapshot) => {
-          const orders = [];
-          let total = 0;
-          let cashTotal = 0;
-          let venmoTotal = 0;
-          let onlineFee = 0;
-          let queue = [];
-
-          querySnapshot.forEach( (order) => {
-
-              orders.push( {
-                  id: order.id,
-                  data: order.data()
-              });
-
-              total += order.data().price
-
-              if( order.data().cash){
-                cashTotal += order.data().price
-              } else {
-                if( order.data().online ){
-                  venmoTotal += (order.data().price - 0.5)
-                  onlineFee += 0.5
-                  total -= 0.5
-                } else {
-                  venmoTotal += order.data().price
-                }
-              }
-              
-              if( !order.data().done ){
-                queue.push(order.data().name);
-              }
-
-          })
-
-          let d = new Date
-
-          if(d.getHours() == 21 || d.getHours() == 22 || d.getHours() == 23 || d.getHours() == 0){ //Adjusts Minutes so totals reflect previous day rather than the next
-            d.setMinutes(d.getMinutes() - 60);
-          }
-          
-          let weekly = (d.getMonth()+ 1) + "-" + d.getDate() + "-totals"
-
-          setDoc( doc( db, "finances",weeklyPrefix,"daily-totals",weekly ), {
-
-            total: total,
-            cashTotal: cashTotal,
-            venmoTotal: venmoTotal,
-            totalOrders: orders.length,
-            onlineFee: onlineFee,
-            date: new Date()
-
-          });
-
-          setDoc( doc(db,"orders","daily-queue"),{
-            queue: queue,
-          });
-
-        commit('SET_ORDERS', {o: orders, t: total, of: onlineFee} )
-
-      })
-
-    },
-
-    async getFinancialData( {commit} ){
-      const q = query( viewCollection.view , orderBy("date") )
-      const pastWeeks = query( operationCollection, orderBy("date") )
-
-      const querySnapshot = await getDocs(q);
-
-      const venmo = []
-      const cash = []
-      const week = []
-
-      let rev = 0
-      let venmoRev = 0
-      let cashRev = 0
-      let onlineFee = 0
-
-
-      querySnapshot.forEach ((order) => {
-
-        venmo.push(
-            order.data().venmoTotal
-        )
-
-        cash.push(
-            order.data().cashTotal
-        )
-
-        week.push(
-            order.id.split("-totals",1)
-        )
-        
-        rev += order.data().total
-        venmoRev += order.data().venmoTotal
-        cashRev += order.data().cashTotal
-        if(order.data().onlineFee){
-          onlineFee += order.data().onlineFee
-        }
-
-      })
-
-      commit("SET_CHART_DATA",{v: venmo, c: cash, w: week, r: rev, vr: venmoRev, cr: cashRev, of: onlineFee})
-    },
-
-    async getStoreData({commit}){
-      const storeRuns = query ( collection(db, "finances", this.state.selectedWeek, "store-runs"), orderBy("date") )
-
-      const storeSnapshot = await getDocs( storeRuns )
-
-      let totalCost = 0
-      const store = []
-
-      storeSnapshot.forEach((storeRun) =>{
-
-        store.push({ 
-          date: storeRun.data().date,
-          cost: storeRun.data().cost,
-          reason: storeRun.data().reason,
-          id: storeRun.id
-        })
-
-        totalCost += storeRun.data().cost
-
-      })
-
-      commit( "SET_STORE_DATA",{s: store, c: totalCost})
-
-    },
-
-    async getWeeksOfOperation({commit}){
-
-      const pastWeeks = query( operationCollection, orderBy("date") )
-      const weeks = []
-
-      const weeksSnapshot = await getDocs(pastWeeks);
-
-      weeksSnapshot.forEach ((week) => {
-        weeks.push(
-            week.id
-        )
-      })
-
-      commit("SET_WEEKS",weeks)
-
-    },
-
-    async getCustomerBase({commit}){
-
-      const customerSnapshot = await getDocs(collection(db,"customers"));
-      const customerBase = []
-
-      customerSnapshot.forEach((customer) => {
-        customerBase.push(
-          customer.id
-        )
-      })
-
-      commit("SET_CUSTOMER_BASE", customerBase)
-
-    },
-
-    async getSchedule( {commit} ){
-      const scheduleQuery = query( collection(db,"finances",this.state.selectedWeek, "schedule"), orderBy("order") )
-      const schedule = []
-
-      const scheduleSnapshot = await getDocs(scheduleQuery)
-
-      scheduleSnapshot.forEach( (day) => {
-        schedule.push(
-          day.data()
-        )
-      })
-
-      commit("SET_SCHEDULE", schedule)
-
-    },
-
-    async getEmployees( {commit} ){
-      const employees = []
-      
-      const employeeSnapshot = await getDocs( collection(db,"employees") )
-
-      employeeSnapshot.forEach((employee) => {
-        employees.push(
-          employee.id
-        )
-      })
-
-      commit("SET_EMPLOYEES",employees)
-
-    },
-
-    async getHours( {commit} ){
-      const hours = []
-      const employees = []
-      let totalHours = 0
-      let hourlyWage = 0
-
-      const hoursSnapshot = await getDocs( collection(db,"finances",this.state.selectedWeek,"hours") )
-
-      hoursSnapshot.forEach((employee) => {
-
-        totalHours += employee.data().hours
-
-        if( employees.includes(employee.data().employee) ){
-          hours[employees.indexOf(employee.data().employee)] += employee.data().hours
-        } else {
-
-          hours.push(
-            employee.data().hours
-          )
-
-          employees.push(
-            employee.data().employee
-          )
-        }
-
-      })
-
-      hourlyWage = 0.9 * (this.state.weeklyRev - this.state.totalCost) / totalHours
-
-      const hourlyPay = hours.map( function(element){
-        return element * hourlyWage
-      })
-
-      commit("SET_HOURS", {h: hours,e: employees, t: totalHours, p: hourlyPay})
-
-
-    },
-
-    addStoreRun({commit}, storeRun){
-      commit("ADD_STORE_RUN",storeRun)
-    },
-
-    addCost({commit}, cost){
-      commit("INCREMENT_COST",cost)
-    },
-
-    deleteStoreRun({commit}, id){
-      commit("DEL_STORE_RUN",id)
-    },
-
-    changeWeek({commit}, week){
-      commit("CHANGE_SELECTED_WEEK",week)
-    },
 
     async getWeeksSQL( {commit} ){
       const response = await axios.post('https://duncan-grille-api.azurewebsites.net/api/get-orders',{sql: 'SELECT * from weeks order by start_date desc;'})
